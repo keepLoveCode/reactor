ca17fb6744fccc7d04175ec3fcf067813ec81003
grubby@lwork.com <grubby@lwork.com>
2019-11-19 18:25:53 +0800

    flux-demo:array.filter

--- reactor3/src/main/java/com/grubby/reactor/FluxDemo.java
+++ reactor3/src/main/java/com/grubby/reactor/FluxDemo.java
@@ -1,9 +1,7 @@
 package com.grubby.reactor;
 
-import reactor.core.publisher.Flux;
 
 public class FluxDemo {
     public static void main(String[] args) {
-
     }
 }
--- reactor3/src/main/java/com/grubby/reactor/inmitation/FluxFilter.java
+++ reactor3/src/main/java/com/grubby/reactor/inmitation/FluxFilter.java
@@ -1,4 +1,84 @@
 package com.grubby.reactor.inmitation;
 
-public class FluxFilter {
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.function.Predicate;
+
+/**
+ *
+ *
+ *
+ * @param <T>
+ */
+
+//
+public class FluxFilter<T> extends Flux<T> {
+
+    private Predicate<T> predicate;
+
+    private boolean done;
+
+
+    @Override
+    public void subscribe(Subscriber<? super T> subscriber) {
+
+    }
+
+    static class FilterSubscription<T> implements Subscriber<T>,Subscription{
+
+        private Subscriber<T> actual;
+
+        private boolean done;
+
+        private Subscription upstreamOfSubscription;
+
+        private Predicate<T> predicate;
+
+        public FilterSubscription(Subscriber<T> actual,Predicate<T> predicate) {
+            this.actual = actual;
+            this.predicate = predicate;
+        }
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            upstreamOfSubscription = subscription;
+            actual.onSubscribe(this);
+        }
+
+        @Override
+        public void onNext(T t) {
+            if (predicate.test(t)) {
+                actual.onNext(t);
+            }
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            if (done) {
+                return;
+            }
+            done = true;
+            actual.onError(throwable);
+        }
+
+        @Override
+        public void onComplete() {
+            if (done) {
+                return;
+            }
+            done = true;
+            actual.onComplete();
+        }
+
+        @Override
+        public void request(long l) {
+            upstreamOfSubscription.request(l);
+        }
+
+        @Override
+        public void cancel() {
+
+        }
+    }
 }
